f23c4cb3d8e029c537b4a57aafb582ec7f744ea7
Swarnesh2003 <swarnesh9421@gmail.com>
2025-09-16 20:05:50 +0530

    new

--- trial.py
+++ trial.py
@@ -7,6 +7,8 @@ def subtract(a, b):
 def divide(a, b):
      return a/b
 
+def multiply(a, b):
+     return a*b
 a  = int(input("Enter number 1: "))
 b  = int(input("Enter number 2: "))
 print(sum(a, b))
