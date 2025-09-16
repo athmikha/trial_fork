657db6534164dd786ea6e12592705d76d3f9c830
athmikha <102711156+athmikha@users.noreply.github.com>
2025-09-16 19:54:04 +0530

    Update trial.py

--- trial.py
+++ trial.py
@@ -4,6 +4,9 @@ def sum(a, b):
 def subtract(a, b):
      return a-b
 
+def divide(a, b):
+     return a/b
+
 a  = int(input("Enter number 1: "))
 b  = int(input("Enter number 2: "))
 print(sum(a, b))
